d48c55ff071fcc82ab8f3c3147f1872b86743315
Teniel Smith-Edwards <tsmith1227@outlook.com>
2025-04-06 13:50:53 -0400

    lab complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { StudentsComponent } from './students.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    StudentsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
